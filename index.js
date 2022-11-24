99f218491588f4d03610d1e510ef0149cacb0a19
+ <+>
2022-11-24 10:57:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+//code your solutions here
+
+let startBlock=  42;
+let lengthOfBlockHq = 264;
+
+function distanceFromHqInBlocks(blocks) {
+    let result = blocks - startBlock;
+    if (result < 0) { return result *-1}
+     return result;
+
+  }
+  function distanceFromHqInFeet(lengthFromHq) {
+    return distanceFromHqInBlocks(lengthFromHq) * lengthOfBlockHq;
+    
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    if(start > destination){
+        return (start - destination) * lengthOfBlockHq
+    }
+    return (destination - start) * lengthOfBlockHq;
+  }
+
+  function calculatesFarePrice(start, destination) {
+  let distanceTravelled = distanceTravelledInFeet(start, destination)
+  if (distanceTravelled<=400)
+  {return 0}
+  else if(distanceTravelled> 400 && distanceTravelled <2000){
+    return 2.56}
+    else if(distanceTravelled>2000 && distanceTravelled<2500){
+      return 25}
+      else if(distanceTravelled>2500){
+        return"cannot travel that far"
+      }
+    }
+  
--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ describe('index.js', function() {
   describe('distanceFromHqInBlocks()', function() {
     it('returns a distance in blocks', function() {
       expect(distanceFromHqInBlocks(43)).to.equal(1);
-    });
+    });``
 
     it('returns a distance in blocks', function() {
       expect(distanceFromHqInBlocks(50)).to.equal(8);
